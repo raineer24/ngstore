8f730a46dfd9e3d81cf0e958918666f5f63e9530
Raineer <delaritaraineer81@gmail.com>
2018-01-19 14:07:43 +0800

    ngstore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { AppComponent }  from './app.component';
+import { ArticleComponent }  from './components/article.component';
+import { reducers, metaReducers } from './reducers/reducers';
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
 
 @NgModule({
+  imports: [     
+        BrowserModule,
+            StoreModule.forRoot(reducers, {metaReducers}),
+             StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      
+             })
+  ],
   declarations: [
-    AppComponent
+        AppComponent,
+		ArticleComponent
   ],
-  imports: [
-    BrowserModule
+  providers: [
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+        AppComponent
+  ]
 })
 export class AppModule { }
